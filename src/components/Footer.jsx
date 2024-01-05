d1e0ebdb5632ca3e277245776b335fc3fcbea310
Wesam Shujaa <w.shujaa@presspage.com>
2024-01-05 11:30:13 +0100

    fix styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,42 +5,44 @@ import { FaFacebookF, FaYoutube, FaInstagram } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className="footer-container">
-      <div className="social-links">
-        <h3>Social link</h3>
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebookF />
-        </a>
-        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
-          <FaYoutube />
-        </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagram />
-        </a>
+      <div>
+        <div className="footer-links">
+          <h3>Links</h3>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/doctors">Doctors</a>
+            </li>
+            <li>
+              <a href="/appointments">Appointments</a>
+            </li>
+            <li>
+              <a href="/notifications">Notifications</a>
+            </li>
+            <li>
+              <a href="/#contact">Contact Us</a>
+            </li>
+            <li>
+              <a href="/profile">Profile</a>
+            </li>
+          </ul>
+        </div>
+        <div className="social-links">
+          <h3>Social link</h3>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebookF />
+          </a>
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+            <FaYoutube />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+        </div>
       </div>
-      <div className="footer-links">
-        <h3>Links</h3>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/doctors">Doctors</a>
-          </li>
-          <li>
-            <a href="/appointments">Appointments</a>
-          </li>
-          <li>
-            <a href="/notifications">Notifications</a>
-          </li>
-          <li>
-            <a href="/#contact">Contact Us</a>
-          </li>
-          <li>
-            <a href="/profile">Profile</a>
-          </li>
-        </ul>
-      </div>
-      <p class="footer-copyright">&copy; 2024 Health Booker. All rights reserved.</p>
+      <p class="footer-copyright-section">&copy; 2024 Health Booker. All rights reserved.</p>
     </footer>
   );
 };
