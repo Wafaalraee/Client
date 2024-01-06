1dea13f9bae09fbde5d3468d9c0179527af30f5f
Wesam Shujaa <w.shujaa@presspage.com>
2024-01-06 11:51:46 +0100

    fix-styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
+import Header from './components/Header';
 import Navbar from './components/Navbar';
 import LoginPage from "./pages/LoginPage";
 import ClientReg from "./pages/ClientReg";
@@ -13,13 +14,16 @@ const App = () => {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Navbar />
-        <Routes>
-        <Route path="/" element={<HomePage/>} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register/client" element={<ClientReg />} />
-          <Route path="/register/doctor" element={<DoctorReg />} />
-        </Routes>
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register/client" element={<ClientReg />} />
+            <Route path="/register/doctor" element={<DoctorReg />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Contact from './Contact';
 import AboutUs from './AboutUs';
+import YourHealth from "./YourHealth";
 const HomePage = () => {
   return (
-      <div>  
-          <section className="welcome-section">
-            <h2>Your Health, Our Responsibility</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique, ipsum in fermentum...</p>
-          </section>
-          <AboutUs />
-          <Contact />
-        </div>
-   
+    <div>  
+      <YourHealth />
+      <AboutUs />
+      <Contact />
+    </div>
   );
 };
 
